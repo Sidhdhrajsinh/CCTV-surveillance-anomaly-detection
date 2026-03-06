4c68f616fbe041f5aefdc54ceb6c5214e179988d
khushhadwani <hadwanikhush@gmail.com>
2026-03-06 18:14:34 +0530

    add favicon

--- ai_service/main.py
+++ ai_service/main.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI
 from api.routes import router
 from fastapi.staticfiles import StaticFiles
 
-app = FastAPI(title="AI Surveillance System")
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "AI CCTV Surveillance API Running"}
 
 app.include_router(router)
 
-# Serve annotated videos
 app.mount("/videos", StaticFiles(directory="outputs"), name="videos")
